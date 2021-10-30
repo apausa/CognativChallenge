39c718a55b78e71413c4c0a0a1cdbdb8627957f4
pabloapausa <pabloapausa@outlook.com>
2021-10-30 14:21:54 +0200

    App component navigation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import React, { Suspense } from 'react';
 import { RecoilRoot } from 'recoil';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from "./src/screens/home/Home";
 import Loading from "./src/screens/loading/Loading";
+import Details from "./src/screens/details/Details";
 
-const App = () => (
-  <RecoilRoot>
-    <Suspense fallback={<Loading />}>
-      <NavigationContainer>
-        <Home />
-      </ NavigationContainer>
-    </Suspense>
-  </RecoilRoot>
-);
-
-export default App;
+export default function App() {
+  const { Screen, Navigator } = createNativeStackNavigator();
+  return (
+    <RecoilRoot>
+      <Suspense fallback={<Loading />}>
+        <NavigationContainer>
+          <Navigator>
+            <Screen name="Home" component={Home}/>
+            <Screen name="Details" component={Details}/>
+          </Navigator>
+        </ NavigationContainer>
+      </Suspense>
+    </RecoilRoot>
+  );
+}
--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -26,16 +26,13 @@ const data = {
 };
 
 export default function Details() {
-
-  let imageUrl = "http://via.placeholder.com/640x360";
-  if (data && data.photo) imageUrl = data.photo;
   return (
     <SafeAreaView style={styles.mainScreen}>
       <StatusBar barStyle="dark-content" />
       <View style={styles.container}>
         <ScrollView showsVerticalScrollIndicator={false}>
           <View style={styles.imageContainer}>
-            <Image source={{ uri: imageUrl }} style={styles.recipeImage} />
+            <Image source={{ uri: imageUrl(data) }} style={styles.recipeImage} />
             <View style={styles.darkener} />
           </View>
           <View style={styles.infoContainer}>
