2b0b9b9bcb2f5d00dcb977e4c2b60681ba6ddd21
pabloapausa <pabloapausa@outlook.com>
2021-10-31 18:32:39 +0100

    Test Column component

--- src/components/column/column.test.tsx
+++ src/components/column/column.test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react-native';
+
 import Column from './Column';
+import itemMock from '../../mocks/itemMock';
+import navigationMock from '../../mocks/navigationMock';
 
 describe('Given a Column component', () => {
+  let screen: any;
+  beforeEach(() => { screen =
+    render(<Column item={itemMock} navigation={navigationMock}/>);
+  })
   describe('When is rendered', () => {
     test('Then matches snapshot', () => {
-      const column = renderer.create(<Column />).toJSON();
-      expect(column).toMatchSnapshot();
+      expect(screen).toMatchSnapshot();
+    });
+    describe('And you press an item', () => {
+      test('Then calls "navigation.push()"', () => {
+        const item = screen.getByTestId('itemColumn');
+        fireEvent.press(item);
+        expect(navigationMock.push).toHaveBeenCalled();
+      })
     })
   })
 })
